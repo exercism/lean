1c1152a436131936f783647982bca8ece7059299
Eric Willigers <keiravillekode@gmail.com>
2026-01-11 20:03:50 +1100

    isOdd

--- exercises/practice/luhn/.meta/Example.lean
+++ exercises/practice/luhn/.meta/Example.lean
@@ -6,11 +6,11 @@ def value (digit : UInt32) (isOdd : Bool) : UInt32 :=
   else 2 * digit - 9
 
 def validTotal (digits : List Char) : Bool := Id.run do
-  let mut count := 0
+  let mut isOdd := false
   let mut total := 0
   for digit in (List.reverse digits) do
-    count := count + 1
-    total := total + (value (digit.val - 48) (count % 2 != 0))
+    isOdd := !isOdd
+    total := total + (value (digit.val - 48) isOdd)
   return total % 10 == 0
 
 def valid (value : String) : Bool := Id.run do

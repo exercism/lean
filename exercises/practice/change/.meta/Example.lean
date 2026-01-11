cf55d2079834aaa854fd416f8c421cf2933d2b63
oxe-i <oxe-b@gmail.com>
2026-01-11 03:42:32 -0300

    add change exercise

---
+++ exercises/practice/change/.meta/Example.lean
@@ -0,0 +1,40 @@
+namespace Change
+
+inductive Result where
+  | ok : Array Nat -> Result
+  | error : String -> Result
+  deriving BEq, Repr
+
+def findFewestCoins (coins : List Nat) (target : Int) : Result := Id.run do
+  match target.toNat? with
+  | none => return .error "target can't be negative"
+  | some targetNat =>
+    if validTarget : target > 0 then
+      let targetNat := target.toNat
+      let mut memo : Array (Option (Array Nat)) := Array.replicate (targetNat + 1) none
+      let mut assigned := true
+
+      memo := memo.set! 0 (some #[])
+
+      while assigned do
+        assigned := false
+        for i in [:memo.size] do
+          let crtResult := memo[i]!
+          if hasResult : crtResult.isSome then
+            let crtResultValue := crtResult.get hasResult
+            for coin in coins do
+              let sum := i + coin
+              if validSum : sum < memo.size then
+                let sumResult := memo[sum]
+                if sumResult.isNone || crtResultValue.size + 1 < sumResult.get!.size then
+                  memo := memo.set sum (crtResultValue.push coin)
+                  assigned := true
+
+      let result := memo.back!
+      if hasResult : result.isSome then
+        return .ok (result.get hasResult)
+      else
+        return .error "can't make target with given coins"
+    else return .ok #[]
+
+end Change

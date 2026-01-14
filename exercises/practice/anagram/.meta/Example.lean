ffa5c109d9c99ef88ca3f68d13f6ae9a4ea754ee
oxe-i <oxe-b@gmail.com>
2026-01-14 13:11:23 -0300

    correct logic

--- exercises/practice/anagram/.meta/Example.lean
+++ exercises/practice/anagram/.meta/Example.lean
@@ -2,16 +2,17 @@ namespace Anagram
 
 def charMap : String -> Vector Nat 26 :=
   String.foldl (fun map char =>
-    let idx := char.toUpper.toNat - 'A'.toNat
+    let idx := char.toNat - 'A'.toNat
     map.set! idx (map[idx]! + 1)
   ) (Vector.replicate 26 0)
 
 def findAnagrams (subject : String) : List String -> List String :=
-  let subjectCharMap := charMap subject
-  List.filter (fun str =>
-    let candidate := str.toUpper
-    let candidateCharMap := charMap candidate
-    candidate != subject && candidateCharMap == subjectCharMap
+  let upperSubject := subject.toUpper
+  let subjectCharMap := charMap upperSubject
+  List.filter (fun candidate =>
+    let upperCandidate := candidate.toUpper
+    let candidateCharMap := charMap upperCandidate
+    upperCandidate != upperSubject && candidateCharMap == subjectCharMap
   )
 
 end Anagram

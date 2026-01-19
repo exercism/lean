75e590fbb0eb7ec1f39e200c5a0ddd4e23164b69
oxe-i <oxe-b@gmail.com>
2026-01-19 00:08:06 -0300

    add comment

--- exercises/practice/perfect-numbers/PerfectNumbers.lean
+++ exercises/practice/perfect-numbers/PerfectNumbers.lean
@@ -2,6 +2,10 @@ namespace PerfectNumbers
 
 def Positive := { x : Nat // x > 0 }
 
+/-
+  You should define Classification here
+-/
+
 def classify (number : Positive) : Classification :=
   sorry
 

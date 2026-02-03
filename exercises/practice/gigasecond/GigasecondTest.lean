6ba2c163a1e318000a5de0e4e39598764c071519
oxe-i <oxe-b@gmail.com>
2026-02-03 17:57:24 -0300

    remove all macros from gigasecond test file

--- exercises/practice/gigasecond/GigasecondTest.lean
+++ exercises/practice/gigasecond/GigasecondTest.lean
@@ -10,15 +10,15 @@ open Std.Time
 def gigasecondTests : TestSuite :=
   (TestSuite.empty "Gigasecond")
   |>.addTest "date only specification of time" (do
-      return assertEqual datetime("2043-01-01T01:46:40") (Gigasecond.add datetime("2011-04-25T00:00:00")))
+      return assertEqual (PlainDateTime.mk (PlainDate.mk 2043 1 1 (by decide)) (PlainTime.mk 1 46 40 0)) (Gigasecond.add (PlainDateTime.mk (PlainDate.mk 2011 4 25 (by decide)) (PlainTime.mk 0 0 0 0))))
   |>.addTest "second test for date only specification of time" (do
-      return assertEqual datetime("2009-02-19T01:46:40") (Gigasecond.add datetime("1977-06-13T00:00:00")))
+      return assertEqual (PlainDateTime.mk (PlainDate.mk 2009 2 19 (by decide)) (PlainTime.mk 1 46 40 0)) (Gigasecond.add (PlainDateTime.mk (PlainDate.mk 1977 6 13 (by decide)) (PlainTime.mk 0 0 0 0))))
   |>.addTest "third test for date only specification of time" (do
-      return assertEqual datetime("1991-03-27T01:46:40") (Gigasecond.add datetime("1959-07-19T00:00:00")))
+      return assertEqual (PlainDateTime.mk (PlainDate.mk 1991 3 27 (by decide)) (PlainTime.mk 1 46 40 0)) (Gigasecond.add (PlainDateTime.mk (PlainDate.mk 1959 7 19 (by decide)) (PlainTime.mk 0 0 0 0))))
   |>.addTest "full time specified" (do
-      return assertEqual datetime("2046-10-02T23:46:40") (Gigasecond.add datetime("2015-01-24T22:00:00")))
+      return assertEqual (PlainDateTime.mk (PlainDate.mk 2046 10 2 (by decide)) (PlainTime.mk 23 46 40 0)) (Gigasecond.add (PlainDateTime.mk (PlainDate.mk 2015 1 24 (by decide)) (PlainTime.mk 22 0 0 0))))
   |>.addTest "full time with day roll-over" (do
-      return assertEqual datetime("2046-10-03T01:46:39") (Gigasecond.add datetime("2015-01-24T23:59:59")))
+      return assertEqual (PlainDateTime.mk (PlainDate.mk 2046 10 3 (by decide)) (PlainTime.mk 1 46 39 0)) (Gigasecond.add (PlainDateTime.mk (PlainDate.mk 2015 1 24 (by decide)) (PlainTime.mk 23 59 59 0))))
 
 def main : IO UInt32 := do
   runTestSuitesWithExitCode [gigasecondTests]

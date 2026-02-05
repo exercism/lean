ed4edab584db76f2af3f31094f23d68ec71dca66
Eric Willigers <keiravillekode@gmail.com>
2026-02-05 12:03:09 +1100

    test with one rail

--- exercises/practice/rail-fence-cipher/RailFenceCipherTest.lean
+++ exercises/practice/rail-fence-cipher/RailFenceCipherTest.lean
@@ -17,6 +17,10 @@ def railFenceCipherTests : TestSuite :=
       return assertEqual "EXERCISMISAWESOME" (RailFenceCipher.decode ⟨5, by decide⟩ "EIEXMSMESAORIWSCE"))
   |>.addTest "decode -> decode with six rails" (do
       return assertEqual "112358132134558914423337761098715972584418167651094617711286" (RailFenceCipher.decode ⟨6, by decide⟩ "133714114238148966225439541018335470986172518171757571896261"))
+  |>.addTest "encode with one rail" (do
+      return assertEqual "ABC" (RailFenceCipher.encode ⟨1, by decide⟩ "ABC"))
+  |>.addTest "decode with one rail" (do
+      return assertEqual "ABC" (RailFenceCipher.decode ⟨1, by decide⟩ "ABC"))
 
 def main : IO UInt32 := do
   runTestSuitesWithExitCode [railFenceCipherTests]

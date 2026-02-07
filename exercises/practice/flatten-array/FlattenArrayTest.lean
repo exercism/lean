a158c12c1e236aac84545f13369811fe50daf42f
Eric Willigers <keiravillekode@gmail.com>
2026-02-07 21:55:59 +1100

    improve indentation

--- exercises/practice/flatten-array/FlattenArrayTest.lean
+++ exercises/practice/flatten-array/FlattenArrayTest.lean
@@ -18,8 +18,8 @@ def flattenArrayTests : TestSuite :=
   |>.addTest "flattens a nested array" (do
       return assertEqual #[] (FlattenArray.flatten (FlattenArray.Box.many #[
         (FlattenArray.Box.many #[
-        (FlattenArray.Box.many #[])
-      ])
+          (FlattenArray.Box.many #[])
+        ])
       ])))
   |>.addTest "flattens array with just integers present" (do
       return assertEqual #[
@@ -27,13 +27,13 @@ def flattenArrayTests : TestSuite :=
       ] (FlattenArray.flatten (FlattenArray.Box.many #[
         (FlattenArray.Box.one 1),
         (FlattenArray.Box.many #[
-        (FlattenArray.Box.one 2),
-        (FlattenArray.Box.one 3),
-        (FlattenArray.Box.one 4),
-        (FlattenArray.Box.one 5),
-        (FlattenArray.Box.one 6),
-        (FlattenArray.Box.one 7)
-      ]),
+          (FlattenArray.Box.one 2),
+          (FlattenArray.Box.one 3),
+          (FlattenArray.Box.one 4),
+          (FlattenArray.Box.one 5),
+          (FlattenArray.Box.one 6),
+          (FlattenArray.Box.one 7)
+        ]),
         (FlattenArray.Box.one 8)
       ])))
   |>.addTest "5 level nesting" (do
@@ -43,21 +43,21 @@ def flattenArrayTests : TestSuite :=
         (FlattenArray.Box.one 0),
         (FlattenArray.Box.one 2),
         (FlattenArray.Box.many #[
-        (FlattenArray.Box.many #[
-        (FlattenArray.Box.one 2),
-        (FlattenArray.Box.one 3)
-      ]),
-        (FlattenArray.Box.one 8),
-        (FlattenArray.Box.one 100),
-        (FlattenArray.Box.one 4),
-        (FlattenArray.Box.many #[
-        (FlattenArray.Box.many #[
-        (FlattenArray.Box.many #[
-        (FlattenArray.Box.one 50)
-      ])
-      ])
-      ])
-      ]),
+          (FlattenArray.Box.many #[
+            (FlattenArray.Box.one 2),
+            (FlattenArray.Box.one 3)
+          ]),
+          (FlattenArray.Box.one 8),
+          (FlattenArray.Box.one 100),
+          (FlattenArray.Box.one 4),
+          (FlattenArray.Box.many #[
+            (FlattenArray.Box.many #[
+              (FlattenArray.Box.many #[
+                (FlattenArray.Box.one 50)
+              ])
+            ])
+          ])
+        ]),
         (FlattenArray.Box.one (-2))
       ])))
   |>.addTest "6 level nesting" (do
@@ -66,23 +66,23 @@ def flattenArrayTests : TestSuite :=
       ] (FlattenArray.flatten (FlattenArray.Box.many #[
         (FlattenArray.Box.one 1),
         (FlattenArray.Box.many #[
-        (FlattenArray.Box.one 2),
-        (FlattenArray.Box.many #[
-        (FlattenArray.Box.many #[
-        (FlattenArray.Box.one 3)
-      ])
-      ]),
-        (FlattenArray.Box.many #[
-        (FlattenArray.Box.one 4),
-        (FlattenArray.Box.many #[
-        (FlattenArray.Box.many #[
-        (FlattenArray.Box.one 5)
-      ])
-      ])
-      ]),
-        (FlattenArray.Box.one 6),
-        (FlattenArray.Box.one 7)
-      ]),
+          (FlattenArray.Box.one 2),
+          (FlattenArray.Box.many #[
+            (FlattenArray.Box.many #[
+              (FlattenArray.Box.one 3)
+            ])
+          ]),
+          (FlattenArray.Box.many #[
+            (FlattenArray.Box.one 4),
+            (FlattenArray.Box.many #[
+              (FlattenArray.Box.many #[
+                (FlattenArray.Box.one 5)
+              ])
+            ])
+          ]),
+          (FlattenArray.Box.one 6),
+          (FlattenArray.Box.one 7)
+        ]),
         (FlattenArray.Box.one 8)
       ])))
   |>.addTest "null values are omitted from the final result" (do
@@ -117,44 +117,44 @@ def flattenArrayTests : TestSuite :=
         (FlattenArray.Box.one 0),
         (FlattenArray.Box.one 2),
         (FlattenArray.Box.many #[
-        (FlattenArray.Box.many #[
-        (FlattenArray.Box.one 2),
-        (FlattenArray.Box.one 3)
-      ]),
-        (FlattenArray.Box.one 8),
-        (FlattenArray.Box.many #[
-        (FlattenArray.Box.many #[
-        (FlattenArray.Box.one 100)
-      ])
-      ]),
-        FlattenArray.Box.zero,
-        (FlattenArray.Box.many #[
-        (FlattenArray.Box.many #[
-        FlattenArray.Box.zero
-      ])
-      ])
-      ]),
+          (FlattenArray.Box.many #[
+            (FlattenArray.Box.one 2),
+            (FlattenArray.Box.one 3)
+          ]),
+          (FlattenArray.Box.one 8),
+          (FlattenArray.Box.many #[
+            (FlattenArray.Box.many #[
+              (FlattenArray.Box.one 100)
+            ])
+          ]),
+          FlattenArray.Box.zero,
+          (FlattenArray.Box.many #[
+            (FlattenArray.Box.many #[
+              FlattenArray.Box.zero
+            ])
+          ])
+        ]),
         (FlattenArray.Box.one (-2))
       ])))
   |>.addTest "all values in nested array are null" (do
       return assertEqual #[] (FlattenArray.flatten (FlattenArray.Box.many #[
         FlattenArray.Box.zero,
         (FlattenArray.Box.many #[
-        (FlattenArray.Box.many #[
-        (FlattenArray.Box.many #[
-        FlattenArray.Box.zero
-      ])
-      ])
-      ]),
+          (FlattenArray.Box.many #[
+            (FlattenArray.Box.many #[
+              FlattenArray.Box.zero
+            ])
+          ])
+        ]),
         FlattenArray.Box.zero,
         FlattenArray.Box.zero,
         (FlattenArray.Box.many #[
-        (FlattenArray.Box.many #[
-        FlattenArray.Box.zero,
-        FlattenArray.Box.zero
-      ]),
-        FlattenArray.Box.zero
-      ]),
+          (FlattenArray.Box.many #[
+            FlattenArray.Box.zero,
+            FlattenArray.Box.zero
+          ]),
+          FlattenArray.Box.zero
+        ]),
         FlattenArray.Box.zero
       ])))
 
--- generators/Generator/Generator/FlattenArrayGenerator.lean
+++ generators/Generator/Generator/FlattenArrayGenerator.lean
@@ -16,13 +16,26 @@ open LeanTest
 def {exercise.decapitalize}Tests : TestSuite :=
   (TestSuite.empty \"{exercise}\")"
 
+def serializeList2
+  (indent : String)
+  (json : Json)
+  (serializer : Json → String := fun j => s!"{j}")
+    : String :=
+    let separator : String := s!",\n{indent}  "
+    let contents := serializeContent json serializer
+    match contents with
+    | [] => "[]"
+    | xs =>
+      let joined := String.intercalate separator xs
+      s!"[\n{indent}  {joined}\n{indent}]"
+
 partial def serializeInt (num : Json) : String :=
   num |>.getInt? |> getOk |> intLiteral
 
-partial def serializer (array : Json) : String :=
+partial def serializer (indent : String) (array : Json) : String :=
   match array with
   | .num _ => s!"(FlattenArray.Box.one {serializeInt array})"
-  | .arr _ => "(FlattenArray.Box.many #" ++ (serializeList array serializer) ++ ")"
+  | .arr _ => "(FlattenArray.Box.many #" ++ (serializeList2 indent array (serializer (indent ++ "  "))) ++ ")"
   | _ => "FlattenArray.Box.zero"
 
 def genTestCase (exercise : String) (case : TreeMap.Raw String Json) : String :=
@@ -32,7 +45,7 @@ def genTestCase (exercise : String) (case : TreeMap.Raw String Json) : String :=
   let description := case.get! "description"
               |> (·.compress)
   let funName := getFunName (case.get! "property")
-  let call := s!"({exercise}.{funName} {serializer array})"
+  let call := s!"({exercise}.{funName} {serializer indent array})"
   s!"
   |>.addTest {description} (do
       return assertEqual #{serializeList expected serializeInt ", "} {call})"

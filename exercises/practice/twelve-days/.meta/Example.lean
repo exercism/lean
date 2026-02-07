5bf6929230955dfa3d6fe871b0af6a9a859ed120
keiravillekode <keiravillekode@gmail.com>
2026-02-07 22:06:21 +1100

    Add `twelve-days` exercise (#107)

---
+++ exercises/practice/twelve-days/.meta/Example.lean
@@ -0,0 +1,30 @@
+namespace TwelveDays
+
+abbrev VerseIndex := { x : Nat // 1 ≤ x ∧ x ≤ 12 }
+
+def ordinal (verse : Nat) : String :=
+  let ordinals := "firstsecondthirdfourthfifthsixthseventheighthninthtentheleventhtwelfth"
+  let ordinalsTable := #[0, 0, 5, 11, 16, 22, 27, 32, 39, 45, 50, 55, 63, 70]
+  ordinals
+      |> (·.toSlice)
+      |> (·.drop ordinalsTable[verse]!)
+      |> (·.take (ordinalsTable[verse + 1]! - ordinalsTable[verse]!))
+      |> (·.copy)
+
+def gift (verse : Nat) : String :=
+  let gifts := "twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+  let giftsTable := #[262, 235, 213, 194, 174, 157, 137, 113, 90, 69, 48, 26, 0]
+  gifts
+      |> (·.toSlice)
+      |> (·.drop giftsTable[verse]!)
+      |> (·.take (giftsTable[0]! - giftsTable[verse]!))
+      |> (·.copy)
+
+def lyrics (verse : Nat) : String :=
+  s!"On the {ordinal verse} day of Christmas my true love gave to me: {gift verse}"
+
+def recite (startVerse endVerse : VerseIndex) : List String :=
+  List.range' startVerse.val (endVerse.val + 1 - startVerse.val)
+      |> (·.map lyrics)
+
+end TwelveDays
---
+++ generators/Generator/Generator/TwelveDaysGenerator.lean
@@ -0,0 +1,39 @@
+import Lean.Data.Json
+import Std
+import Helper
+
+open Lean
+open Std
+open Helper
+
+namespace TwelveDaysGenerator
+
+def genIntro (exercise : String) : String := s!"import LeanTest
+import {exercise}
+
+open LeanTest
+
+def {exercise.decapitalize}Tests : TestSuite :=
+  (TestSuite.empty \"{exercise}\")"
+
+def genTestCase (exercise : String) (case : TreeMap.Raw String Json) : String :=
+  let input := case.get! "input"
+  let startVerse := input.getObjValD "startVerse" |> (s!"⟨{·}, by decide⟩")
+  let endVerse := input.getObjValD "endVerse" |> (s!"⟨{·}, by decide⟩")
+  let expected := case.get! "expected"
+  let description := case.get! "description"
+              |> (·.compress)
+  let funName := getFunName (case.get! "property")
+  let call := s!"({exercise}.{funName} {startVerse} {endVerse})"
+  s!"
+  |>.addTest {description} (do
+      return assertEqual {serializeList expected} {call})"
+
+def genEnd (exercise : String) : String :=
+  s!"
+
+def main : IO UInt32 := do
+  runTestSuitesWithExitCode [{exercise.decapitalize}Tests]
+"
+
+end TwelveDaysGenerator
